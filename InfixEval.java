b5b91607a0f067b40a2c2ec4df69f0d1428e425a
Edilson Hernandez <edilson.hernandez1@gmail.com>
2018-03-25 22:49:03 -0400

    Add method to solve arithmetic, perform arithmetic when right parentheses is detected

--- InfixEval.java
+++ InfixEval.java
@@ -2,23 +2,61 @@ import java.util.*;
 
 public class InfixEval {
   public static void Evaluate(String s){
-    Stack operand = new Stack();
-    Stack operator = new Stack();
+    int result = 0;
+    Stack<Integer> operand = new Stack<>();
+    Stack<String> operator = new Stack<>();
     // add space around operators and parentheses to make splitting simpler
     String formattedS = s.replaceAll("\\+", " + ").replaceAll("\\-", " - ").replaceAll("\\*", " * ").replaceAll("\\/", " / ").replaceAll("\\(", " ( ").replaceAll("\\)", " ) ");
     String[] tokens = formattedS.split(" ");
-    System.out.println(formattedS);
     for (int i = 0;i<tokens.length;i++) {
       if (isInteger(tokens[i])) {
-        operand.push(tokens[i]);
-      } else {
+        operand.push(Integer.parseInt(tokens[i]));
+      } else if(tokens[i].equals("(")) {
+        operator.push(tokens[i]);
+      } else if (tokens[i].equals(")")) {
+        while (String.valueOf(operator.lastElement()) != "(") {
+          int secondOperand = operand.pop();
+          int firstOperand = operand.pop();
+          String opr = operator.pop();
+          result = getResult(firstOperand, secondOperand, opr);
+          if (operand.isEmpty()) {
+            operand.push(result);
+            break;
+          }
+          operand.push(result);
+        }
+        operator.pop();
+      } else if (tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
         operator.push(tokens[i]);
       }
     }
-    String ggg = Arrays.toString(operand.toArray());
-    System.out.println("Operands: " + ggg);
-    String qqq = Arrays.toString(operator.toArray());
-    System.out.println("Operators: " + qqq);
+    while (!operator.isEmpty()) {
+      int secondOperand = operand.pop();
+      int firstOperand = operand.pop();
+      String opr = operator.pop();
+      result = getResult(firstOperand, secondOperand, opr);
+      operand.push(result);
+    }
+    System.out.println(operand.pop());
+  }
+
+  public static int getResult(int x, int y, String s) {
+    int rslt = 0;
+    switch (s) {
+      case "+":
+        rslt = x + y;
+        break;
+      case "-":
+        rslt = x - y;
+        break;
+      case "*":
+        rslt = x * y;
+        break;
+      case "/":
+        rslt = x / y;
+        break;
+    }
+    return rslt;
   }
 
   public static boolean isInteger(String n){
