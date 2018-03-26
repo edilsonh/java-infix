5ddb296553be9a52971760a0403da326690fa8ec
Edilson Hernandez <edilson.hernandez1@gmail.com>
2018-03-26 02:00:58 -0400

    Solve order of precedence in parentheses

--- InfixEval.java
+++ InfixEval.java
@@ -2,6 +2,8 @@ import java.util.*;
 import javax.swing.*;
 
 public class InfixEval {
+  private static ArrayList<Integer> num = new ArrayList<>();
+  private static ArrayList<String> op = new ArrayList<>();
   public static void Evaluate(String s){
     int result = 0;
     Stack<Integer> operand = new Stack<>();
@@ -16,48 +18,75 @@ public class InfixEval {
         operator.push(tokens[i]);
       } else if (tokens[i].equals(")")) {
         while (String.valueOf(operator.lastElement()) != "(") {
-          int secondOperand = operand.pop();
-          int firstOperand = operand.pop();
-          String opr = operator.pop();
-          result = getResult(firstOperand, secondOperand, opr);
+          int oprnd = operand.pop();
+          String oprtr = operator.pop();
+          preparePrecedence(oprnd, oprtr);
           if (operand.isEmpty()) {
+            result = getResult();
             operand.push(result);
             break;
           }
-          operand.push(result);
         }
-        operator.pop();
       } else if (tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
         operator.push(tokens[i]);
       }
     }
     while (!operator.isEmpty()) {
-      int secondOperand = operand.pop();
       int firstOperand = operand.pop();
       String opr = operator.pop();
-      result = getResult(firstOperand, secondOperand, opr);
-      operand.push(result);
+      preparePrecedence(firstOperand, opr);
     }
+    result = getResult();
+    operand.push(result);
     System.out.println(operand.pop());
   }
 
-  public static int getResult(int x, int y, String s) {
+  public static void preparePrecedence(int x, String s){
+    if (num.size() == 0) {
+        num.add(x);
+    } else {
+      num.add(0, x);
+    }
+    if (op.size() == 0) {
+      op.add(s);
+    } else {
+      op.add(0, s);
+    }
+  }
+
+  public static int getResult() {
     int rslt = 0;
+    System.out.println(op.indexOf("+"));
     try {
-      switch (s) {
-        case "+":
-          rslt = x + y;
-          break;
-        case "-":
-          rslt = x - y;
-          break;
-        case "*":
-          rslt = x * y;
-          break;
-        case "/":
-          rslt = x / y;
-          break;
+      while (op.indexOf("*") != -1) {
+        int ind = op.indexOf("*");
+        int multiply = num.get(ind-1) * num.get(ind);
+        num.remove(ind);
+        num.set(ind-1, multiply);
+        op.remove(ind);
       }
+      while (op.indexOf("/") != -1) {
+        int ind = op.indexOf("/");
+        int division = num.get(ind-1) / num.get(ind);
+        num.remove(ind);
+        num.set(ind-1, division);
+        op.remove(ind);
+      }
+      while (op.indexOf("+") != -1) {
+        int ind = op.indexOf("+");
+        int addition = num.get(ind-1) + num.get(ind);
+        num.remove(ind);
+        num.set(ind-1, addition);
+        op.remove(ind);
+      }
+      while (op.indexOf("-") != -1) {
+        int ind = op.indexOf("-");
+        int subtract = num.get(ind-1) - num.get(ind);
+        num.remove(ind);
+        num.set(ind-1, subtract);
+        op.remove(ind);
+      }
+      rslt = num.get(0);
     } catch (Exception e) {
       System.out.println(e);
       System.out.println("Cannot divide by zero!");
